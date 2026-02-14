a4000fc04e2697c14c501c053bbd241c9a0e74b4
lamarasingham@icloud.com <lamarasingham@icloud.com>
2026-02-14 02:52:22 +0000

    Created db and Form1; Edited settings, theme/parameters.yaml and custom package dependencies

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,26 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+import anvil.server
+import m3.components as m3
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    x=anvil.server.call_s('getconn')
+    self.connect.text=x
+    # Any code you write here will run before the form opens.
+
+  @handle("submit", "click")
+  def submit_click(self, **event_args):
+    """This method is called when the component is clicked."""
+    x=anvil.server.call('execute_query',self.connect.text,self.query.text)
+    if isinstance(x,str):
+     self.output.content+='\n'+x
+    else:
+      self.output.content+='\n'+str(x)
+
+  @handle("connect", "change")
+  def connect_change(self, **event_args):
+    """This method is called when the text in this component is edited."""
+    anvil.server.call_s('conn',self.connect.text)
---
+++ server_code/db.py
@@ -0,0 +1,43 @@
+import anvil.server
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+# Get connection string from Anvil Secrets
+@anvil.server.callable
+def getconn():
+  try:
+    x=anvil.server.cookies.shared['conn']
+  except:
+    x=''
+  if x:
+    return x
+  else:
+    return ''
+    
+@anvil.server.callable
+def conn(x):
+  anvil.server.cookies.shared['conn']=x
+
+def get_connection(x):
+  # SSL mode 'require' is mandatory for Neon
+  return psycopg2.connect(x, sslmode='require')
+
+@anvil.server.callable
+def execute_query(db, query, params=None):
+  """Executes a SQL query and returns results as a list of dicts."""
+  try:
+    with get_connection(db) as conn:
+      with conn.cursor(cursor_factory=RealDictCursor) as cur:
+        cur.execute(query, params)
+        if cur.description: # If query returns data (like SELECT)
+          return list(cur.fetchall())
+        conn.commit()
+        return "Success"
+  except Exception as e:
+    return f"Error: {str(e)}"
+
+@anvil.server.callable
+def update_row(db, table, column, value, row_id):
+  """Specific function for an editor to update a cell."""
+  query = f"UPDATE {table} SET {column} = %s WHERE id = %s"
+  return execute_query(db, query, (value, row_id))
