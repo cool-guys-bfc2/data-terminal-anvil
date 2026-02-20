caef0babda4e94316862162bef93af28dd91f71c
lamarasingham@icloud.com <lamarasingham@icloud.com>
2026-02-20 00:45:48 +0000

    Edited Richtext

--- client_code/Richtext/__init__.py
+++ client_code/Richtext/__init__.py
@@ -8,10 +8,16 @@ class Richtext(RichtextTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    self.text_area_1.text=anvil.server.call_s('getconn','html.index')
     # Any code you write here will run before the form opens.
 
   @handle("timer_1", "tick")
   def timer_1_tick(self, **event_args):
     """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
     self.refresh_data_bindings()
+
+  @handle("text_area_1", "change")
+  def text_area_1_change(self, **event_args):
+    """This method is called when the text in this component is edited."""
+    x=self.text_area_1.text
+    anvil.server.call_s('conn',x)
